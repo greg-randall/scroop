948eee1e7165420ef9d85453f9b6ee1e5f14a362
gregrr@gmail.com <gregrr@gmail.com>
2024-06-03 10:29:23 -0400

    added multithreaded page caching, added caching of summary and match info, tweaks to make all that work

--- scroop.py
+++ scroop.py
@@ -20,8 +20,9 @@ from urllib.parse import quote
 import os
 import random
 import shutil
-
-
+from tqdm import tqdm
+from concurrent.futures import ThreadPoolExecutor
+import hashlib
 
 
 
@@ -32,6 +33,16 @@ now = datetime.now()
 timestamp = now.strftime("%m-%d-%Y_%I-%M-%p")
 
 output_filename = f"job_search_{timestamp}.csv"
+
+# Check if the log file exists, if it doesn't, create it
+# The log file is used to keep track of which sites have been scanned
+if not os.path.exists("scanned_sites.log"):
+    open("scanned_sites.log", 'w').close()
+
+#create the cache folder if it doesn't exist
+if not os.path.exists('cached_pages'):
+    os.makedirs('cached_pages')
+
 
 # Initialize an empty list to store the links
 links = []
@@ -56,7 +67,9 @@ for count, (search_site, search_word) in enumerate([(site, word) for site in sea
     
     # Print a message indicating that the page is being fetched
     # The total number of pages is the product of the number of search sites and search words
-    print(f"\r{count}/{len(search_sites)*len(search_words)} -- {page}", end="", flush=True)
+    #print(f"\r{count}/{len(search_sites)*len(search_words)} -- {page}", end="", flush=True)
+    print(f"\r\033[K{count}/{len(search_sites)*len(search_words)} -- {page}", end="", flush=True)
+
     
     # Fetch the content of the page
     # The second argument to get_page_content is the maximum age of cached content in hours
@@ -81,16 +94,41 @@ for count, (search_site, search_word) in enumerate([(site, word) for site in sea
             cprint("\tNo page content retrieved","red")
 
 # Print the total number of links collected, in green
-print(f"\n\nTotal Links: {len(links)}\n")
+print(f"\n\nTotal Links Found: {len(links)}")
+
+# Read the file 'scanned_sites.log' into a list
+with open('scanned_sites.log', 'r') as file:
+    scanned_sites = file.read().splitlines()
+# Remove any items from 'links' that exist in 'scanned_sites'
+links = [link for link in links if link not in scanned_sites]
+
+
+print(f"Links Remaining after Previously Scanned removed: {len(links)}\n\n")
+
+
+# Shuffle the list of links to ensure randomness
+random.shuffle(links)
+
+####
+#Make sure pages are cached in parallel
+####
+print("Caching pages in parallel...\n")
+
+# Create a ThreadPoolExecutor
+with ThreadPoolExecutor(max_workers=8) as executor:
+    # Use the executor to start a new thread for each link
+    # Wrap the call to executor.map with tqdm to show a progress bar
+    page_contents = list(tqdm(executor.map(get_page_content, links, [720]*len(links)), total=len(links)))
+del page_contents
+
+####
+#generate summaries in parallel
 
 ####
 #Scan the generated links for job relevance
 ####
 
-print("Scanning for jobs relevance...\n")
-# Shuffle the list of links to ensure randomness
-random.shuffle(links)
-
+print("\nScanning for jobs relevance...\n")
 
 # Check if the output file exists, if it doesn't, create it
 # The output file is used to store the results of the job search
@@ -99,16 +137,9 @@ if not os.path.exists(output_filename):
         # Write the header row to the output file
         file.write('timestamp,url,job is a good match with resume\n')
 
-# Check if the log file exists, if it doesn't, create it
-# The log file is used to keep track of which sites have been scanned
-if not os.path.exists("scanned_sites.log"):
-    open("scanned_sites.log", 'w').close()
 
 # Loop through the shuffled links
 for count, link in enumerate(links, start=1):
-
-    job_match = False #initialize job_match to False
-    add_job_to_csv = False
 
     # Print the current link number and the link itself and make sure the link is not too long
     columns, _ = shutil.get_terminal_size()
@@ -117,10 +148,10 @@ for count, link in enumerate(links, start=1):
 
     # Open the log file and read its content
     with open("scanned_sites.log", 'r') as file:
-        content = file.read()
+        previously_scanned_sites = file.read()
 
     # Check if the link is not in the log file, does not start with any of the search sites, and does not contain "keywords"
-    if link not in content and not any(link.startswith(search_site) for search_site in search_sites) and "keywords" not in link:
+    if link not in previously_scanned_sites: #and not any(link.startswith(search_site) for search_site in search_sites) and "keywords" not in link:
         # Fetch the content of the page at the link
         page_content_raw = get_page_content(link, 720) # keep reusing cached pages for 30 days (720 hours = 30 days)
         # Extract the body text from the page content
@@ -134,23 +165,50 @@ for count, link in enumerate(links, start=1):
             found_word = find_keywords(page_content, search_words)
 
             if found_word:
-                add_job_to_csv = True
-
+  
                 # Ask the user to read the job listing and write a summary of required skills and degrees
                 prompt = f"Please read this job listing and write a consise summary of required skills, degrees, etc:\n\n{compress_prompt(page_content)}"
 
-                # Use the LLM to generate a summary of the job listing
-                job_summary = gpt_me(prompt,"gpt-3.5-turbo",open_ai_key)
+                filename = f"{hashlib.md5(link.encode()).hexdigest()}_summary.txt"
+                filepath = os.path.join('cached_pages', filename)
+                if not os.path.exists(filepath):
+                    # Use the LLM to generate a summary of the job listing
+                    job_summary = gpt_me(prompt,"gpt-3.5-turbo",open_ai_key)
+                    with open(filepath, 'w') as file:
+                        file.write(job_summary)
+                else:
+                    with open(filepath, 'r') as file:
+                        job_summary = file.read()
 
+
+                #print("looking at good fit")
                 # Ask the user to determine if the job is a good match with the resume
-                prompt = f"Read the applicant's RESUME and JOB SUMMARY below and determine if the applicant is a good fit for this job on a scale of 1 to 10. 1 is a bad fit, 10 is a perfect fit.\n\nJOB SUMMARY:  {compress_prompt(bullet_resume)}\n\nJOB SUMMARY:  {compress_prompt(job_summary)}"
-                job_is_a_good_match = gpt_range(prompt,"gpt-4o", open_ai_key)
+                prompt = f"Read the applicant's RESUME and JOB SUMMARY below and determine if the applicant is a good fit for this job on a scale of 1 to 10. 1 is a bad fit, 10 is a perfect fit. REPLY WITH AN INTEGER 1-10!!!\n\nJOB SUMMARY:  {compress_prompt(bullet_resume)}\n\nJOB SUMMARY:  {compress_prompt(job_summary)}"
+                #print(f"looking at good fit:\n{prompt}\n\n")
 
-                # If the job is relevant and a good match, print a success message
-                if job_is_a_good_match>=5:
-                    cprint(f"\n\nJOB FOUND!!! -- {link}\n","green")
-                with open(output_filename, 'a') as file:
-                    file.write(f'{datetime.now().strftime("%m/%d/%Y %I:%M %p")},{link},{job_is_a_good_match}\n')
+                filename = f"{hashlib.md5(link.encode()).hexdigest()}_rating.txt"
+                filepath = os.path.join('cached_pages', filename)
+                if not os.path.exists(filepath):
+                    # Use the LLM to generate a summary of the job listing
+                    job_is_a_good_match = gpt_range(prompt,"gpt-4o", open_ai_key,True)
+                    with open(filepath, 'w') as file:
+                        file.write(str(job_is_a_good_match))
+                else:
+                    with open(filepath, 'r') as file:
+                        job_is_a_good_match = file.read()
+
+                
+
+
+                if isinstance(job_is_a_good_match,int):
+
+                    with open(filepath, 'w') as file:
+                        file.write(str(job_is_a_good_match))
+                    # If the job is relevant and a good match, print a success message
+                    if job_is_a_good_match>=5:
+                        cprint(f"\n\nJOB FOUND!!! -- {link}\n","green")
+                    with open(output_filename, 'a') as file:
+                        file.write(f'{datetime.now().strftime("%m/%d/%Y %I:%M %p")},{link},{job_is_a_good_match}\n')
 
             # Append the link to the log file, so we know to skip it in the future, and to avoid duplicates
             with open("scanned_sites.log", 'a') as file:
